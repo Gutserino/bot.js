a8cbef3f68ed79a9d995ebd59bdbf7cb8680a4fa
Gutserino <55332780+Gutserino@users.noreply.github.com>
2019-09-15 01:41:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+var url = 'http://mcapi.us/server/status?ip=Gutserino88.aternos.me';
  
 
 client.on('ready', () => {
@@ -17,7 +17,35 @@ client.on("message", (message) => {
   if (!message.content.startsWith(prefix)) return;
  
   if (message.content.startsWith(prefix + "status")) {
-    message.channel.send("pong!");
+    request(url, function(err, response, body) {
+      if(err) {
+          console.log(err);
+          //return message.reply('Error getting Minecraft server status...');
+      }
+      body = JSON.parse(body);
+      var status = 'Server offline';
+      console.log(body.motd);
+      if(body.online) {
+          if((body.motd=="&cWe are under maintenance.")||(body.players.now>=body.players.max)){
+            message.channel.send("Offline");
+            //.then(console.log)
+            .catch(console.error);
+          }else{
+            message.channel.send("Online");
+            //.then(console.log)
+            .catch(console.error);
+          }
+            if(body.players.now) {
+                status = ' ' + body.players.now + '  of  ' + body.players.max;
+              } else {
+                status = ' 0  of  ' + body.players.max;
+        }
+      } else {
+        message.channel.send("Offline");
+        //.then(console.log)
+        .catch(console.error);
+
+      }
   } else
   if (message.content.startsWith(prefix + "foo")) {
     message.channel.send("bar!");
