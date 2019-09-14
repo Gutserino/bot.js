5a0730759fecf60127d33e00bb5499e8f6ce28e0
Gutserino <55332780+Gutserino@users.noreply.github.com>
2019-09-15 01:31:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on("message", (message) => {
   // Exit and stop if it's not there
   if (!message.content.startsWith(prefix)) return;
  
-  if (message.content.startsWith(prefix + "ping")) {
+  if (message.content.startsWith(prefix + "status")) {
     message.channel.send("pong!");
   } else
   if (message.content.startsWith(prefix + "foo")) {
