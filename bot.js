0efcb057de4a6f9e55a50cc3619a914580a332b9
Gutserino <55332780+Gutserino@users.noreply.github.com>
2019-09-15 03:26:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-var facts = [
-    "1",
-    "2",
-    "3",
+var quotes = [
+    "Ich habe große Achtung vor den deutschen Soldaten. In Wirklichkeit sind die Deutschen das einzige anständige in Europa lebende Volk. George S. Patton",
+    "Die Deutschen sind unfähig, aus der Geschichte zu lernen.Genauso wie sie 1939 Hitler in den Krieg folgten, so würden sie Heute in ihrer Blindheit, Merkel in den Abgrund folgen. Wladimir Wladimirowitsch Putin",
+    "Verbrenne ihre Synagogen, zwinge sie zur Arbeit und gehe mit ihnen nach aller Unbarmherzigkeit um. Martin Luther",
+    "Luther war ein großer Mann, ein Riese. Mit einem Ruck durchbrach er die Dämmerung: sah den Juden, wie wir ihn erst heute zu sehen beginnen. Adolf Hitler",
+    "Herrgott, gib uns die Kraft, daß wir uns die Freiheit erhalten, unserem Volk, unseren Kindern und unseren Kindeskindern, nicht nur uns Deutschen, sondern auch den anderen Völkern Europas. Denn es ist nicht ein Krieg, den wir alle dieses Mal führen nur für unser deutsches Volk allein, es ist ein Krieg für ganz Europa und damit wirklich für die ganze Menschheit.",
+    "Denn nur ein Volk ohne brot hört auf die befreier aus seiner not und erkennt im völkerherd seines Vaterlandes wert und den der die freiheit uns verwehrt.",
     ];
 
 client.on('ready', () => {
-    client.user.setActivity('Celebration Bunker');
     console.log('Jawohl, mein Führer!');
 
 });
@@ -20,11 +22,11 @@ client.on("message", (message) => {
   // Exit and stop if it's not there
   if (!message.content.startsWith(prefix)) return;
  
-  if (message.content.startsWith(prefix + "status")) {
-    message.channel.send("pong!");
+  if (message.content.startsWith(prefix + "commands")) {
+    message.channel.send("!quote -Displays a Random Quote" + \n "!blalbla");
   } else
-  if (message.content.startsWith(prefix + "joke")) {
-    message.channel.send(facts[Math.floor(Math.random() * facts.length)]);
+  if (message.content.startsWith(prefix + "quote")) {
+    message.channel.send(quotes[Math.floor(Math.random() * quotes.length)]);
   }
 
 });
